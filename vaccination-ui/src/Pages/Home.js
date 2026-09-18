9a6dd3b0cf37d5eeb7bf27701cf73800d6ca3112
adityakonapala <adithyakonapala@gmail.com>
2026-09-18 12:40:00 +0530

    Updated Landing page

--- vaccination-ui/src/Pages/Home.js
+++ vaccination-ui/src/Pages/Home.js
@@ -3,7 +3,15 @@ import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className="container-fluid p-0">
+    <div className="container-fluid p-0"
+    style={{
+        minHeight: "100vh",
+        backgroundImage: "url('/vaccination-bg.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+      >
 
       {/* Navbar */}
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary px-4">
@@ -16,6 +24,7 @@ function Home() {
             className="btn btn-light me-2"
             to="/user-login"
           >
+            <span className="bi bi-person"></span>
             User Login
           </Link>
 
@@ -23,6 +32,7 @@ function Home() {
             className="btn btn-light me-2"
             to="/register"
           >
+            <span className="bi bi-person-add"></span>
             User Register
           </Link>
 
@@ -36,27 +46,28 @@ function Home() {
       </nav>
 
       {/* Landing Page Content */}
-      <div className="container text-center mt-5">
+      <div className="container text-center " style={{ marginTop:"200px" }}>
 
-        <h1 className="display-4 fw-bold">
+        <h1 className="display-4 fw-bold ">
           Welcome to Vaccination Portal
         </h1>
 
-        <p className="lead mt-3">
+        <p className="lead mt-4">
           Book your vaccination slot easily and securely.
         </p>
 
         <div className="mt-4">
           <Link
             to="/user-login"
-            className="btn btn-primary btn-lg me-3"
+            className="btn btn-success btn-lg me-3 rounded-pill"
           >
-            Login
+            <span className="bi bi-door-open"></span>
+            Login  
           </Link>
 
           <Link
             to="/register"
-            className="btn btn-outline-primary btn-lg"
+            className="btn btn-outline-primary btn-lg rounded-pill"
           >
             Register
           </Link>
